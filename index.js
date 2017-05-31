98b2aaae904102a75a3d12ef20a5ebfdf42aad35
AMOfficial <karen@g-tea.am>
2017-05-31 21:39:40 +0400

    Configured basic TCP server, client

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const net = require('net');
+const socket = net.Socket();
 const program = require('commander');
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
+
 
 //Parsing CLI passed arguments
 program
@@ -13,6 +18,53 @@ program
 //Setting default arguments if empty
 const parsedArguments = {
   service: (program.service) ? program.service : "client",
-  location: (program.location) ? program.location : "location",
+  location: (program.location) ? program.location : "localhost",
   port: (program.port) ? program.port : "5000"
+}
+
+
+
+//TODO: Separate into modules
+if(parsedArguments.service == "server") {
+
+  //Start a TCP Server
+  net.createServer( sock => {
+    // Show socket object ID -> Unique
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    // Data event handler for this socket
+    sock.on('data', data => {
+        console.log('Data ' + sock.remoteAddress + ': ' + data);
+
+        // Send data back to client socket
+        sock.write('Data sent: "' + data + '"');
+
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', data => {
+        console.log('Closed: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+  }).listen(parsedArguments.port, parsedArguments.location);
+
+}else{
+
+  //Connecting to socket
+  socket.connect(parsedArguments.port, parsedArguments.location, () => {
+      //Send message to socket server
+      socket.write('Hey');
+  });
+
+  //Event for receiving data from server
+  socket.on('data', data => {
+      console.log('Data: ' + data);
+      // Close the client socket
+      socket.destroy();
+  });
+
+  // Add a 'close' event handler for the client socket
+  socket.on('close', () => {
+      console.log('Connection closed');
+  });
 }
