3216696b64b9d2e0f800dc8e5d94d91e874ee329
JohnFordBR <nice.knyazyan@bk.ru>
2017-06-12 06:26:23 +0400

    bug fix 2.0

--- index.js
+++ index.js
@@ -1,173 +1,180 @@
- 'use strict';
+'use strict';
 const net = require('net');
 const socket = net.Socket();
 const program = require('commander');
 const blessed = require('blessed');
 const contrib = require('blessed-contrib');
-let clients = [];
+
 
 
 
 // Basic layout for blessed
-
 const screen = blessed.screen({
-  autoPadding: true,
-    smartCSR: true
+ autoPadding: true,
+   smartCSR: true
 });
 
 
 
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
+ return process.exit(0);
 });
 
 
 //Parsing CLI passed arguments
 program
-  .version('0.0.1')
-  .option('-s, --service [optional]', 'Add service')
-  .option('-l, --location [optional]', 'Location of server')
-  .option('-p, --port [optional]', 'Location of server')
-  .parse(process.argv);
+ .version('0.0.1')
+ .option('-s, --service [optional]', 'Add service')
+ .option('-l, --location [optional]', 'Location of server')
+ .option('-p, --port [optional]', 'Location of server')
+ .parse(process.argv);
 
 //Setting default arguments if empty
 const parsedArguments = {
-  service: (program.service) ? program.service : "client",
-  location: (program.location) ? program.location : "localhost",
-  port: (program.port) ? program.port : "5000"
+ service: (program.service) ? program.service : "client",
+ location: (program.location) ? program.location : "localhost",
+ port: (program.port) ? program.port : "5000"
 }
 
 
 
-//TODO: Separate into modules
+
 if(parsedArguments.service == "server") {
-  //Start a TCP Server
-  net.createServer(sock => {
-      clients.push(sock);
-      sock.on('data', data => {
-        if(JSON.parse(data).type==='nickname')
-        {
-          sock.nickname=JSON.parse(data).nickname;
-          sock.nickcolor='#2300ff';
-        }else  if(JSON.parse(data).type==='nickcolor'){
+ let clients = [];
+ net.createServer(sock => {
+
+     clients.push(sock);
+     sock.on('data', data => {
+       /*if(JSON.parse(data).type==='nickname')
+       {
+         sock.nickname=JSON.parse(data).nickname;
+         sock.nickcolor='#2300ff';
+       }else  if(JSON.parse(data).type==='nickcolor'){
 sock.nickcolor=JSON.parse(data).nickcolor;
-        }
-        clients.forEach(client => {
-          try {
-        if(JSON.parse(data).type!='nickcolor')
-        {
-        client.write(JSON.stringify({message:JSON.parse(data).message,nickname:sock.nickname,nickcolor:sock.nickcolor}));
-        }
-    }catch(err){
+       }*/
+       switch (JSON.parse(data).type) {
+  case 'nickname':
+  sock.nickname=JSON.parse(data).nickname;
+  sock.nickcolor='#2300ff';
+    break;
+  case 'nickcolor':
+  sock.nickcolor=JSON.parse(data).nickcolor;
+    break;
+  default:
+JSON.parse(data).type=='newmessage';
+}
+       clients.forEach(client => {
+         try {
+      if(JSON.parse(data).type!='nickcolor')
+       {
+       client.write(JSON.stringify({message:JSON.parse(data).message,nickname:sock.nickname,nickcolor:sock.nickcolor}));
+       }
+   }catch(err){
 
+   }
+ });
+ });
 
-    }
-  });
-  });
-
-  }).listen(parsedArguments.port, parsedArguments.location);
+ }).listen(parsedArguments.port, parsedArguments.location);
 
 }else{
-  socket.connect(parsedArguments.port, parsedArguments.location, () => {
-  const log = contrib.log(
-        { fg: "green"
-        , label: 'Chat window'
-        , height: "20%"
-        , tags: true
-        , border: {type: "line", fg: "cyan"} });
-    screen.append(log);
+ socket.connect(parsedArguments.port, parsedArguments.location, () => {
+ const log = contrib.log(
+       { fg: "green"
+       , label: 'Chat window'
+       , height: "20%"
+       , tags: true
+       , border: {type: "line", fg: "cyan"} });
+   screen.append(log);
 
-  const chatlog = contrib.log(
-        { fg: "green"
-        , top: '25%'
-        , label: 'messages'
-        , height: "40%"
-        , tags: true
-        ,track: {bg: 'yellow'}
-        , border: {type: "line", fg: "cyan"} });
-        screen.append(chatlog);
-  const form = blessed.form({
-      parent: log,
-      name: 'form',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-  });
-  const input = blessed.textarea({
-      parent: form,
-      name: 'input',
-      inputOnFocus: true,
-      input: true,
-      keys: true,
-      top: 0,
-      left: 0,
-      height: 1,
-      width: '100%',
-      style: {
-          fg: 'white',
-          bg: 'black',
-          focus: {
-              bg: 'red',
-              fg: 'white'
-          }
-      }
-  });
-  const prompt = blessed.Prompt({
-    name: 'prompt',
-    top:'70%',
-    width:'30%',
-    inputOnFocus: true,
-    input: true,
-    keys: true,
-    border: {type: "line", fg: "cyan"}
-  });
-  const bar = blessed.listbar({
-    bottom: 0,
-    height:'20%',
-    left: 3,
-    right: 3,
-    mouse: true,
-    keys: true,
-    autoCommandKeys: true,
-    border: 'line',
-    vi: true,
-    style: {
-      bg: 'green',
-      item: {
-        bg: 'red',
-        hover: {
-          bg: 'blue'
-        },
-        //focus: {
-        //  bg: 'blue'
-        //}
-      },
-      selected: {
-        bg: 'blue'
-      }
-    },
-    commands: {
-      'red' :{
-        callback: function() {
-          socket.write(JSON.stringify({type:'nickcolor',nickcolor:'#f72929'}));
-          bar.hide();
-            input.focus();
-          }
-      },
-      'blue': function() {
-        socket.write(JSON.stringify({type:'nickcolor',nickcolor:'#2300ff'}));
-        bar.hide();
-          input.focus();
-      },
-      'green': function() {
-        socket.write(JSON.stringify({type:'nickcolor',nickcolor:'#e8ff00'}));
-        bar.hide();
-          input.focus();
-      }
-    }
-  });
+ const chatlog = contrib.log(
+       { fg: "green"
+       , top: '25%'
+       , label: 'messages'
+       , height: "40%"
+       , tags: true
+       ,track: {bg: 'yellow'}
+       , border: {type: "line", fg: "cyan"} });
+       screen.append(chatlog);
+ const form = blessed.form({
+     parent: log,
+     name: 'form',
+     top: 0,
+     left: 0,
+     width: '100%',
+     height: '100%',
+ });
+ const input = blessed.textarea({
+     parent: form,
+     name: 'input',
+     inputOnFocus: true,
+     input: true,
+     keys: true,
+     top: 0,
+     left: 0,
+     height: 1,
+     width: '100%',
+     style: {
+         fg: 'white',
+         bg: 'black',
+         focus: {
+             bg: 'red',
+             fg: 'white'
+         }
+     }
+ });
+ const prompt = blessed.Prompt({
+   name: 'prompt',
+   top:'70%',
+   width:'30%',
+   inputOnFocus: true,
+   input: true,
+   keys: true,
+   border: {type: "line", fg: "cyan"}
+ });
+ const bar = blessed.listbar({
+   bottom: 0,
+   height:'20%',
+   left: 3,
+   right: 3,
+   mouse: true,
+   keys: true,
+   autoCommandKeys: true,
+   border: 'line',
+   vi: true,
+   style: {
+     bg: 'green',
+     item: {
+       bg: 'red',
+       hover: {
+         bg: 'blue'
+       },
+     },
+     selected: {
+       bg: 'blue'
+     }
+   },
+   commands: {
+     'red' :{
+       callback: function() {
+         socket.write(JSON.stringify({type:'nickcolor',nickcolor:'#f72929'}));
+         bar.hide();
+           input.focus();
+         }
+     },
+     'blue': function() {
+       socket.write(JSON.stringify({type:'nickcolor',nickcolor:'#2300ff'}));
+       bar.hide();
+         input.focus();
+     },
+     'green': function() {
+       socket.write(JSON.stringify({type:'nickcolor',nickcolor:'#e8ff00'}));
+       bar.hide();
+         input.focus();
+     }
+   }
+ });
 screen.append(bar);
 bar.toggle();
 screen.append(prompt);
@@ -180,24 +187,24 @@ socket.write(JSON.stringify({type:'nickname',message:'has joined to chat',nickna
 
 
 input.key('enter', ()=>{
-  socket.write(JSON.stringify({message:input.getValue()}));
-  input.clearValue();
-  });
+ socket.write(JSON.stringify({message:input.getValue()}));
+ input.clearValue();
+ });
 
 socket.on('data', data=>{
-  const {nickcolor,nickname,message}= JSON.parse(data);
-  chatlog.log(` {${nickcolor}-fg}${nickname}{/} ${message}`);
+ const {nickcolor,nickname,message}= JSON.parse(data);
+ chatlog.log(` {${nickcolor}-fg}${nickname}{/} ${message}`);
 
-  });
+ });
 
 
 });
 
 
 
-  }
+ }
 
 
 
-//Rendering our UI :)
+
 screen.render();
